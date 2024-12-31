62144de989a227a71cae61b6c27f6e90d6b1db5c
FarruXbek Raxmonov <83349809+raxmonovrx@users.noreply.github.com>
2024-12-31 17:48:00 +0500

    Uzum Market clone

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export const App = () => {
+	return (
+		<div className='h-screen flex items-center justify-center'>
+			<h1 className='font-mono font-bold text-2xl bg-gradient-to-r from bg-sky-500 to-blue-800'>
+				Uzum Market
+			</h1>
+		</div>
+	)
+}
